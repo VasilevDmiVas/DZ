f548f4e042af756d2f5bc49029a9a42444be1ba8
Дмитрий <vasja8102@mail.ru>
2023-05-23 16:38:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# 6.22 г
+# number = int(input('Введите число\n'))
+# suma = 0
+# while number > 0:
+#     num = number % 10
+#     if num > 5:
+#         suma = suma + num
+#     number = number // 10
+# print('Сумма чисел больше 5 =', suma)
+
+# 6.22 д
+# number = int(input('Введите число\n'))
+# proizved = 1
+# while number > 0:
+#     num = number % 10
+#     if num > 7:
+#         proizved = proizved * num
+#     number = number // 10
+# print('Произведение чисел больше 7 =', proizved)
+
+# 6.23 a
+# number = int(input('Введите число\n'))
+# number1 = int(input('Введите число\n'))
+# CountNumber = 0
+# while number > 0:
+#     num = number % 10
+#     if number1 == num:
+#         CountNumber = CountNumber + 1
+#     number = number // 10
+# print(number1, 'встретилась ',CountNumber, 'раз')
+
+# 6.23 в
+# number = int(input('Введите число\n'))
+# number1 = int(input('Введите число a:\n'))
+# CountNumber = 0
+# while number > 0:
+#     num = number % 10
+#     if number1 and num >= 9:
+#         CountNumber = CountNumber + num
+#     number = number // 10
+# print('Сумма = ', CountNumber)
+
+# 6.27 б
+
+# number = int(input('Введите число\n'))
+# maxNumber = 0
+# minNumber = 0
+# while number > 0:
+#     num = number % 10
+#     if num > maxNumber:
+#         maxNumber = num
+#     else:
+#         minNumber = num
+#     number = number // 10
+# c = maxNumber - minNumber
+# print('Максимальное число =', maxNumber)
+# print('Минимальное число =', minNumber)
+# print('На', c, 'максимальное превышает минимальное')
+
+# 6.10 a
+# number = int(input('Введите число\n'))
+# num = 0
+# while num ** 2 < number:
+#     num = num + 1
+#     print(num)
